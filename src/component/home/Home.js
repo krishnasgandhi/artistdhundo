420f9cdb89d1a2541dc82ba4ca87fd1d802afa1c
Krishna Gandhi <krishna.gandhi@v2solutions.com>
2022-06-15 17:24:13 +0530

    react js configuration

---
+++ src/component/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../header/Header';
+import './home.scss';
+
+ 
+
+export default function Home() {
+  return (
+    <>
+    <Header/>
+    <div>Home</div>
+    </>
+  )
+}
