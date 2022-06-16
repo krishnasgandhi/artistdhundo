1274fb58031d5606765048e05855f73f3d3bce5e
Theanarasan-ccm <theanarasan.d@v2solutions.com>
2022-06-16 16:55:39 +0530

    New file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import './App.css';
-import Home from './component/home/Home';
-import CastingCall from './component/casting-call/CastingCall'
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './Components/Home/Home'
+import About from './Components/About/About';
 
 function App() {
   return (
-
     <Router>
-          <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/cast"><CastingCall /></Route>
-</Switch>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+      </Routes>
     </Router>
   );
 }
---
+++ src/Components/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './About.scss';
+import Header from '../Header/Header'
+
+function About() {
+  return (
+    <>
+    <Header />
+    <div>About</div>
+    </>
+  )
+}
+
+export default About
